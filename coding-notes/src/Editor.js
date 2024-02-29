77c0b840637513f4142d5ab802ea65cf680d1a66
Aldo Tavoletti <aldotavoletti@gmail.com>
2024-02-29 18:55:03 +0100

    .

--- coding-notes/src/Editor.js
+++ coding-notes/src/Editor.js
@@ -13,10 +13,13 @@ import Quote from '@editorjs/quote';
 import Warning from '@editorjs/warning';
 import Alert from 'editorjs-alert';
 import { URL } from "./utils";
+import ChangeCase from 'editorjs-change-case';
 import useSWR from "swr";
+import TextVariantTune from '@editorjs/text-variant-tune';
 import { patchAjaxCall } from "./utils";
 import React, { useEffect, useRef } from "react";
-
+import Tooltip from 'editorjs-tooltip';
+import DragDrop from 'editorjs-drag-drop';
 // we use react memo cause when i wirte in the title, the editor gets re-rendered. React.memo makes sure this doesn't happen
 const Editor = React.memo(({currentNote}) => {
 
@@ -46,9 +49,10 @@ const Editor = React.memo(({currentNote}) => {
             /** 
              * Available Tools list. 
              * Pass Tool's class or Settings object for each Tool you want to use 
-             */
-            tools: {
-                header: Header,
+            */
+           tools: {
+               header: Header,
+               textVariant: TextVariantTune,
                 Marker: {
                     class: ColorPlugin, // if load from CDN, please try: window.ColorPlugin
                     config: {
@@ -56,22 +60,33 @@ const Editor = React.memo(({currentNote}) => {
                         type: 'marker',
                         icon: `<svg fill="#000000" height="200px" width="200px" version="1.1" id="Icons" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M17.6,6L6.9,16.7c-0.2,0.2-0.3,0.4-0.3,0.6L6,23.9c0,0.3,0.1,0.6,0.3,0.8C6.5,24.9,6.7,25,7,25c0,0,0.1,0,0.1,0l6.6-0.6 c0.2,0,0.5-0.1,0.6-0.3L25,13.4L17.6,6z"></path> <path d="M26.4,12l1.4-1.4c1.2-1.2,1.1-3.1-0.1-4.3l-3-3c-0.6-0.6-1.3-0.9-2.2-0.9c-0.8,0-1.6,0.3-2.2,0.9L19,4.6L26.4,12z"></path> </g> <g> <path d="M28,29H4c-0.6,0-1-0.4-1-1s0.4-1,1-1h24c0.6,0,1,0.4,1,1S28.6,29,28,29z"></path> </g> </g></svg>`
                     }
-                }, Color: {
-                    class: ColorPlugin, // if load from CDN, please try: window.ColorPlugin
+                }, changeCase: {
+                    class: ChangeCase,
                     config: {
-                        colorCollections: ['#EC7878', '#9C27B0', '#673AB7', '#3F51B5', '#0070FF', '#03A9F4', '#00BCD4', '#4CAF50', '#8BC34A', '#CDDC39', '#FFF'],
-                        defaultColor: '#FF1300',
-                        type: 'text',
-                        customPicker: true // add a button to allow selecting any colour  
+                        showLocaleOption: true, // enable locale case options
+                        locale: 'tr' // or ['tr', 'TR', 'tr-TR']
                     }
                 },
                 title: Title,
                 warning: Warning,
                 quote: Quote,
                 alert: Alert,
+               tooltip: {
+                   class: Tooltip,
+                   config: {
+                       location: 'left',
+                       underline: true,
+                    //    placeholder: 'Enter a tooltip',
+                       highlightColor: '#FFEFD5',
+                       backgroundColor: '#154360',
+                       textColor: '#FDFEFE',
+                       holder: 'editorjs',
+                   }
+               },
                 paragraph: {
                     class: Paragraph,
                     inlineToolbar: true,
+                    tunes:["textVariant"]
                 },
                 list: {
                     class: NestedList,
@@ -122,6 +137,12 @@ const Editor = React.memo(({currentNote}) => {
 
 
 
+            },
+
+            onReady: ()=>{
+
+
+                new DragDrop(editor);
             }
 
         });
