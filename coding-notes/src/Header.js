d980de369769702e08d5bbcb24f234285b88e4a0
Aldo Tavoletti <aldotavoletti@gmail.com>
2024-04-02 09:27:44 +0200

    .

--- coding-notes/src/Header.js
+++ coding-notes/src/Header.js
@@ -6,7 +6,7 @@ import useSWR from "swr";
 const Header = ({ modalShowing, setModalShowing, currentNote, noteTitle, setNoteTitle, isLoggedIn, setIsLoggedIn }) => {
 
     const isPatching = useRef(false);
-    
+
     const fetcher = (...args) => fetch(...args).then((res) => res.json());
     const { data: note, isValidating, isLoading, error } = useSWR(URL + `?retrieve=single&note=${currentNote}`, fetcher, { revalidateOnFocus: false });
 
@@ -17,9 +17,9 @@ const Header = ({ modalShowing, setModalShowing, currentNote, noteTitle, setNote
 
         }
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [note]);
-    
+
     useEffect(() => {
 
         if (note && !isPatching.current && noteTitle !== note.title) /* if the note has been fetched and there is no patch operation ongoing */ {
@@ -32,27 +32,37 @@ const Header = ({ modalShowing, setModalShowing, currentNote, noteTitle, setNote
         //? the eslint error has got to be disabled cause adding "note" to the dependancy list causes problems
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [noteTitle]);
-        // Handles error and loading state. Without these useSWR doesn't work
-        if (error) return (<div></div>);
-        // if (!note || isLoading || isValidating) return (<div className="header"></div>);
+    // Handles error and loading state. Without these useSWR doesn't work
+    if (error) return (<div></div>);
+    // if (!note || isLoading || isValidating) return (<div className="header"></div>);
 
-   const handleTitleInput = (e)=>{
+    const handleTitleInput = (e) => {
 
         switchState(noteTitle, setNoteTitle, e.currentTarget.innerText);
-   }
+    };
     return (
 
         <div className="header">
-            
-            {(!note && (isValidating || isLoading)) && <p></p>}
-            {(!note && !isValidating && !isLoading) && <p>Coding Notes</p>}
-            { note && <p contentEditable="true" suppressContentEditableWarning={ true } onDragStart={ (e) => e.preventDefault() } data-placeholder="Title..." className="note-display__title" onKeyDown={ (e) => (e.currentTarget.innerText.length > 50 && /^.$/.test(e.key)) && e.preventDefault()}onInput={ (e) => handleTitleInput(e)}>{note.title}</p> }
-            
-            <div className="header__buttons-div">
 
-                 { isLoggedIn && <button className="primary-button" onClick={ () => switchState(modalShowing, setModalShowing, "folder") }>Add a folder +</button>}
-                { isLoggedIn && <button className="primary-button" onClick={ () => switchState(modalShowing, setModalShowing, "note") }>Add a note +</button> }
-                
+            { (!note && (isValidating || isLoading)) && <p></p> }
+            { (!note && !isValidating && !isLoading) && <p>Coding Notes</p> }
+            { note && <p contentEditable="true" suppressContentEditableWarning={ true } onDragStart={ (e) => e.preventDefault() } data-placeholder="Title..." className="note-display__title" onKeyDown={ (e) => (e.currentTarget.innerText.length > 50 && /^.$/.test(e.key)) && e.preventDefault() } onInput={ (e) => handleTitleInput(e) }>{ note.title }</p> }
+
+            <div className="header__buttons-div">
+                { isLoggedIn &&
+
+                    (
+                        <>
+                        <button className="text-button" onClick={ () => switchState(isLoggedIn, setIsLoggedIn)}>logout</button> 
+                        <div className="vert-line"></div>
+                            <button className="primary-button" onClick={ () => switchState(modalShowing, setModalShowing, "folder") }>Add a folder +</button>
+                            <button className="primary-button" onClick={ () => switchState(modalShowing, setModalShowing, "note") }>Add a note +</button>
+                        </>
+                    )
+
+                }
+
+
 
             </div>
         </div>
