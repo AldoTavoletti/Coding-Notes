7b7713e9e3bf3cc74a57fe1777b512c9ba5ed3ce
Aldo Tavoletti <aldotavoletti@gmail.com>
2024-04-11 17:06:33 +0200

    Update Header.js

--- coding-notes/src/Header.js
+++ coding-notes/src/Header.js
@@ -4,48 +4,51 @@ import { simplePatchCall } from "./utils";
 import useSWR from "swr";
 const Header = ({ currentNote, noteTitle, setNoteTitle, isLoggedIn }) => {
 
-    const isPatching = useRef(false);
+    // I use an header ref to get its offsetWidth
     const header = useRef();
+
     const fetcher = (...args) => fetch(...args).then((res) => res.json());
     const { data: note, isValidating, isLoading } = useSWR(URL + `?retrieve=single&note=${currentNote}`, fetcher, { revalidateOnFocus: false });
 
-    useEffect(() => /* this is called at the first render and when the note gets fetched */ {
+    useEffect(() => {
 
-        if (note && noteTitle !== note.title) /* if the note has been fetched */ {
-            setNoteTitle(note.title);
-
-        }
+        // if the note had been fetched and noteTitle is not equal to note.title, set it to be
+        (note && noteTitle !== note.title) && setNoteTitle(note.title);
 
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [note]);
 
+
     useEffect(() => {
 
-        if (note && !isPatching.current && noteTitle !== note.title) /* if the note has been fetched and there is no patch operation ongoing */ {
+        note && simplePatchCall({ noteID: note.noteID, title: noteTitle });
 
-            isPatching.current = true;
-            simplePatchCall({ noteID: note.noteID, title: noteTitle });
-            isPatching.current = false;
-
-        }
-        //? the eslint error has got to be disabled cause adding "note" to the dependancy list causes problems
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [noteTitle]);
-    
-    const handleTitleInput = (e) => {
-
-        setNoteTitle(e.currentTarget.innerText);
-    };
 
     return (
 
-        <div className="header" ref={header}>
+        <div className="header" ref={ header }>
 
+            {/* if no note has been selected or if the login page is shown */ }
             { (!isLoggedIn || (!note && !isValidating && !isLoading)) && <p>Coding Notes</p> }
-            { (!note && (isValidating || isLoading)) && <p></p> }
-            { note && isLoggedIn && currentNote && <p contentEditable="true" suppressContentEditableWarning={ true } onDragStart={ (e) => e.preventDefault() } data-placeholder="Title..." className="note-display__title" onKeyDown={ (e) => (e.currentTarget.offsetWidth > (header.current.offsetWidth /1.2) && /^.$/.test(e.key)) && e.preventDefault() } onInput={ (e) => handleTitleInput(e) }>{ note.title }</p> }
 
-           
+            {/* if the selected note is validating */ }
+            { (!note && (isValidating || isLoading)) && <p></p> }
+
+            {/* if a note has been selected and it's been fetched */ }
+            { (note && currentNote) &&
+                <p
+                    contentEditable="true"
+                    suppressContentEditableWarning={ true }
+                    onDragStart={ (e) => e.preventDefault() }
+                    data-placeholder="Title..."
+                    className="note-display__title"
+                    onKeyDown={ (e) => (e.currentTarget.offsetWidth > (header.current.offsetWidth / 1.2) && /^.$/.test(e.key)) && e.preventDefault() }
+                    onInput={ (e) => setNoteTitle(e.currentTarget.innerText) }
+                >{ note.title }</p> }
+
+
         </div>
 
     );
