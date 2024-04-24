4c68d8540854a847b743cd46eb0e0a315a5c90c1
Aldo Tavoletti <aldotavoletti@gmail.com>
2024-04-24 19:05:57 +0200

    .

--- api/db_connection.php
+++ api/db_connection.php
@@ -4,9 +4,13 @@ $servername = "codingnotes.c9isugac825d.eu-north-1.rds.amazonaws.com";
 $username = "aldotavoletti"; 
 $password = 'KSd{{eY40DyMv$RvK7}s0i_Pf0NB';
 $dbname = "codingnotesdb"; 
- 
+// $servername = "localhost";
+// $username = "root";
+// $password = '';
+// $dbname = "codingnotesdb";
+
 // Create connection 
-$conn = mysqli_connect($servername, $username, $password, $dbname, 3306); 
+$conn = mysqli_connect($servername, $username, $password, $dbname); 
  
 // Check connection 
 if (!$conn) { 
