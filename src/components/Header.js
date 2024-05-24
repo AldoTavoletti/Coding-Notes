d53a23fcf5965fc5c1896de6b32eebad0de6b5d1
Aldo Tavoletti <aldotavoletti@gmail.com>
2024-05-24 12:09:17 +0200

    .

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import { useEffect, useRef } from "react";
 import { URL } from "../utils/utils";
 import { simplePatchCall, logout } from "../utils/utils";
 import useSWR from "swr";
+
 const Header = ({ currentNote, noteTitle, setNoteTitle, isLoggedIn, setIsLoggedIn, menuStatus, setMenuStatus }) => {
 
     // I use an header ref to get its offsetWidth
@@ -25,15 +26,11 @@ const Header = ({ currentNote, noteTitle, setNoteTitle, isLoggedIn, setIsLoggedI
 
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [noteTitle]);
-    
-    
+
+
     return (
 
         <div className="header" ref={ header }>
-
-
-
-
 
             {/* if no note has been selected or if the login page is shown */ }
             { (!isLoggedIn || (!note && !isValidating && !isLoading)) && <p>Coding Notes</p> }
@@ -46,25 +43,25 @@ const Header = ({ currentNote, noteTitle, setNoteTitle, isLoggedIn, setIsLoggedI
                 <p
                     contentEditable="true"
                     suppressContentEditableWarning={ true }
-                    onDragStart={ (e) => e.preventDefault() }
+                    // onDragStart={ (e) => e.preventDefault() }
                     data-placeholder="Title..."
-                    className="note-display__title"
+                    className="header__title"
                     // onKeyDown={ (e) => (e.currentTarget.offsetWidth > (header.current.offsetWidth / 1.2) && /^.$/.test(e.key)) && e.preventDefault() }
                     onInput={ (e) => setNoteTitle(e.currentTarget.innerText) }
                 >{ note.title }</p> }
 
             <div className="main-header__buttons-div">
-                { isLoggedIn && 
-                <div className="dropdown">
-                    <button className="account-button" type="button" data-bs-toggle="dropdown">{ isLoggedIn[0].toUpperCase() }</button>
-                    <ul className="dropdown-menu">
-                            <li><h6 className="dropdown-header">Hi {isLoggedIn}!</h6></li>
+                { isLoggedIn &&
+                    <div className="dropdown">
+                        <button className="account-button" type="button" data-bs-toggle="dropdown">{ isLoggedIn[0].toUpperCase() }</button>
+                        <ul className="dropdown-menu">
+                            <li><h6 className="dropdown-header">Hi { isLoggedIn }!</h6></li>
                             <li><button onClick={ () => logout(setIsLoggedIn, false) }>Logout</button></li>
                             <li><button data-bs-toggle="modal" data-bs-target="#deleteAccountModal">Delete account</button></li>
-                            
-                    </ul>
-                </div>
-}
+
+                        </ul>
+                    </div>
+                }
                 { menuStatus === "hamburger" &&
 
                     <i className="bi bi-list" onClick={ () => setMenuStatus("expanded") }></i>
