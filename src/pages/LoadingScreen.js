37483cfbc61ec8ca7f67b7b2bf346cd44bcc5974
Aldo Tavoletti <aldotavoletti@gmail.com>
2024-06-01 21:48:40 +0200

    .

--- src/pages/LoadingScreen.js
+++ src/pages/LoadingScreen.js
@@ -1,12 +1,20 @@
 const LoadingScreen = () => {
-    return ( 
+    return (
 
-        <div className="center-container">
-            <div className="spinner-grow" role="status">
+        <div className="loading-screen">
+            Coding N&nbsp;&nbsp;&nbsp;
+            <div className="ripple-background">
+
+                <div className="circle xxlarge shade1"></div>
+                <div className="circle xlarge shade2"></div>
+                <div className="circle large shade3"></div>
+                <div className="circle medium shade4"></div>
+
             </div>
+        tes
         </div>
 
-     );
-}
- 
+    );
+};
+
 export default LoadingScreen;
