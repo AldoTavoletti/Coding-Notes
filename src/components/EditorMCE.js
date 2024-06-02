f886ce730e825dadb07f0f1595c437d8063151ef
Aldo Tavoletti <aldotavoletti@gmail.com>
2024-06-02 14:29:48 +0200

    a

--- src/components/EditorMCE.js
+++ src/components/EditorMCE.js
@@ -142,10 +142,10 @@ const EditorMCE = ({ currentNote }) => {
                         --tertiary-bg-color-hover: #ffffffc9;
                         --tertiary-bg-color-active: #ffffff8f;
                         --quaternary-bg-color: #1d1d1d;
-                        --purple: #5941df;
-                        --purple-light: #7e64ff;
-                        --purple-hover: #4a33ce;
-                        --purple-active: #31247d;
+                        --purple: #6b41bf;
+    --purple-light: #9b64df;
+    --purple-hover: #5326a8;
+    --purple-active: #3e247d;
                         --text-color: white;
                         --text-color-secondary: black;
                         --editor-bg-color: #1f1f1f;
@@ -166,7 +166,8 @@ const EditorMCE = ({ currentNote }) => {
                         --quaternary-bg-color: #c0c0c0;
                         --text-color: black;
                         --text-color-secondary: white;
-                        --purple-active: #31247d;
+                            --purple-active: #3e247d;
+
                         --editor-bg-color: white;
                     }
                    /* Changes the color of the placeholder */
