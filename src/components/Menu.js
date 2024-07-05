93c355f93509f122ca76742a2bb3cb45a1cc9557
Aldo Tavoletti <aldotavoletti@gmail.com>
2024-07-05 13:52:15 +0200

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -29,7 +29,7 @@ const Menu = ({ menuStatus, setMenuStatus, currentNote, setCurrentNote, setModal
         }),
         useSensor(TouchSensor,{
 
-            activationConstraint:{distance:20}
+            activationConstraint:{distance:5}
 
         }),
         useSensor(KeyboardSensor, {
