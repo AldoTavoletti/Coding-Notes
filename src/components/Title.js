82fccd8867b5a68a731ddd4870ab3bb9771cd4b0
Aldo Tavoletti <aldotavoletti@gmail.com>
2024-07-14 14:58:53 +0200

    Update Title.js

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,14 +1,15 @@
+import { useCallback, useRef } from "react";
 import { simplePatchCall, debounce } from "../utils/utils";
 
 const Title = ({ note, currentNote, setNoteTitle, isLoading, isValidating, noteTitle }) => {
 
+    const currentID = useRef(null);
 
-    const handleOnInput = debounce((value) => {
+    const handleOnInput = useCallback(debounce((value) => {
 
-        simplePatchCall({ noteID: currentNote.noteID, title: value });
-        setNoteTitle(value);
+        simplePatchCall({ noteID: currentID.current, title: value });
 
-    });
+    }),[]);
 
     return (
 
@@ -23,6 +24,9 @@ const Title = ({ note, currentNote, setNoteTitle, isLoading, isValidating, noteT
                 spellCheck="false"
                 className="header--note__title"
                 onInput={ (e)=>{
+                    currentID.current = currentNote.noteID;
+                    setNoteTitle(e.currentTarget.innerText);
+
                     handleOnInput(e.currentTarget.innerText);
                 }
                 
