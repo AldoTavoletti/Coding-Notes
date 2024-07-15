23c6098cf6ed474c2b22cd8689c42b9bad612bc1
Aldo Tavoletti <aldotavoletti@gmail.com>
2024-07-15 17:42:22 +0200

    .

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -24,7 +24,7 @@ import {
 import {
     sortableKeyboardCoordinates,
 } from '@dnd-kit/sortable';
-import { saveLastNoteTitle, switchNote, simplePatchCall } from "../utils/utils";
+import { saveLastNoteTitle, switchNote, simplePatchCall, collapseFolders } from "../utils/utils";
 
 const NoteList = ({ setFolders, folders, currentNote, setCurrentNote, menuStatus, setMenuStatus, setModalShowing, noteTitle, setNoteTitle, contextMenuInfo, setContextMenuInfo }) => {
     // used to settle the title of the last note, after another note has been clicked on
@@ -62,57 +62,7 @@ const NoteList = ({ setFolders, folders, currentNote, setCurrentNote, menuStatus
 
 
     };
-    /**
-    * @note collapse all the open folders using classes
-    */
-    const collapseFolders = () => {
-        // get all the collapsible divs of open accordions
-        const accordionCollapseDivs = document.querySelectorAll(".show");
-
-        // get all the buttons of open accordions
-        const accordionButtons = document.querySelectorAll("button.accordion-button:not(.collapsed)");
-
-        const n = accordionCollapseDivs.length;
-
-
-        for (let i = 0; i < n; i++) {
-
-            // collapse the folder
-            accordionCollapseDivs[i].classList.remove("show");
-
-            // make the accordion button arrow change direction
-            accordionButtons[i].classList.add("collapsed");
-
-
-        }
-
-
-
-    };
-
-    /**
-    * @note expand all the open folders using classes
-    */
-    const expandFolders = () => {
-
-        // get all the expandible divs of open accordions
-        const accordionCollapseDivs = document.querySelectorAll(".collapse:not(.show)");
-
-        // get all the buttons of closed accordions
-        const accordionButtons = document.querySelectorAll("button.accordion-button.collapsed");
-
-        const n = accordionButtons.length;
-
-        for (let i = 0; i < n; i++) {
-
-            // expand the folder
-            accordionCollapseDivs[i].classList.add("show");
-
-            // make the accordion button arrow change direction
-            accordionButtons[i].classList.remove("collapsed");
-        }
-
-    };
+    
     const handleDragEnd = (e) => {
         const { active, over } = e;
         active.id = parseInt(active.id);
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -133,7 +133,57 @@ export const openMenu = (e, setMethod, element) => {
     }
 
 };
+/**
+  * @note collapse all the open folders using classes
+  */
+export const collapseFolders = () => {
+    // get all the collapsible divs of open accordions
+    const accordionCollapseDivs = document.querySelectorAll(".show");
 
+    // get all the buttons of open accordions
+    const accordionButtons = document.querySelectorAll("button.accordion-button:not(.collapsed)");
+
+    const n = accordionCollapseDivs.length;
+
+
+    for (let i = 0; i < n; i++) {
+
+        // collapse the folder
+        accordionCollapseDivs[i].classList.remove("show");
+
+        // make the accordion button arrow change direction
+        accordionButtons[i].classList.add("collapsed");
+
+
+    }
+
+
+
+};
+
+/**
+* @note expand all the open folders using classes
+*/
+export const expandFolders = () => {
+
+    // get all the expandible divs of open accordions
+    const accordionCollapseDivs = document.querySelectorAll(".collapse:not(.show)");
+
+    // get all the buttons of closed accordions
+    const accordionButtons = document.querySelectorAll("button.accordion-button.collapsed");
+
+    const n = accordionButtons.length;
+
+    for (let i = 0; i < n; i++) {
+
+        // expand the folder
+        accordionCollapseDivs[i].classList.add("show");
+
+        // make the accordion button arrow change direction
+        accordionButtons[i].classList.remove("collapsed");
+    }
+
+};
 /**
  * 
  * @param {Object} obj
